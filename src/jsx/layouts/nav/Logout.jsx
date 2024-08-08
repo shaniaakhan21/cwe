05f4c6fe67c5adcb3465c2a09b4e1a248e9d27bd
Shaniya Khan <57680699+shaniaakhan21@users.noreply.github.com>
2024-08-08 15:20:45 +0400

    Exchange form changes and logout changes

--- src/jsx/layouts/nav/Logout.jsx
+++ src/jsx/layouts/nav/Logout.jsx
@@ -30,14 +30,14 @@ function LogoutPage(){
             <button  className="dropdown-item ai-icon ms-1" onClick={onLogout}>
                 <svg
                   id="icon-logout" xmlns="http://www.w3.org/2000/svg"
-                  className="text-danger logout" width={18} height={18} viewBox="0 0 24 24" 
+                  className="text-white logout" width={18} height={18} viewBox="0 0 24 24" 
                   fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round"
                 >
                     <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
                     <polyline points="16 17 21 12 16 7" />
                     <line x1={21} y1={12} x2={9} y2={12} />
                 </svg>
-                <span className="ms-2 text-danger" >Logout </span>
+                <span className="ms-2 text-white" >Logout </span>
             </button>
         </>
     )
