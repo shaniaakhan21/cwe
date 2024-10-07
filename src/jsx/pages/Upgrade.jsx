bc4e72aa11cb9d2d85eb6a47369cf5371fbf6c64
Shaniya Khan <shaniaakhan21@gmail.com>
2024-10-07 13:28:47 +0400

    fix mobile 05

--- src/jsx/pages/Upgrade.jsx
+++ src/jsx/pages/Upgrade.jsx
@@ -87,7 +87,7 @@ const UpgradeComponent = () => {
                         "Secure wallet",
                         "Networking & Follow Leaders",
                         "AI Solutions",
-                        "Get Started Easily Today with \nour Beginner Plan"
+                        "Get Started Easily Today with our\nBeginner Plan"
                     ]}
                     buttonText="Start Trading" />
                 <Upgrade
