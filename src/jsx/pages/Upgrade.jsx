12b49a0303bc9203bcadc8c5cb1a19666ce5a904
Shaniya Khan <shaniaakhan21@gmail.com>
2024-10-07 13:07:45 +0400

    fix mobile 02

--- src/jsx/pages/Upgrade.jsx
+++ src/jsx/pages/Upgrade.jsx
@@ -31,7 +31,15 @@ const Upgrade = ({ heading, subheading, listItems, buttonText }) => {
                 <h3>{heading}</h3>
                 <ol>
                     {listItems.map((item, index) => (
-                        <li key={index}>{item}</li>
+                        <li key={index}>
+                            {/* Render the string split by line breaks */}
+                            {item.split('\n').map((line, i) => (
+                                <span key={i}>
+                                    {line}
+                                    <br />
+                                </span>
+                            ))}
+                        </li>
                     ))}
                 </ol>
                 <Button disabled={sending} onClick={handleUpgrade} className="me-2" variant="success" style={{ marginTop: 20 }}>
@@ -42,14 +50,12 @@ const Upgrade = ({ heading, subheading, listItems, buttonText }) => {
     );
 };
 
-
 Upgrade.propTypes = {
     heading: PropTypes.string.isRequired,
     subheading: PropTypes.string,
     listItems: PropTypes.arrayOf(PropTypes.string).isRequired,
     buttonText: PropTypes.string.isRequired,
 };
-
 
 const UpgradeComponent = () => {
     return (
@@ -59,22 +65,56 @@ const UpgradeComponent = () => {
                 <Upgrade
                     heading="Free"
                     subheading="Beginner"
-                    listItems={["Real-time dashboard", "Access to Multiple exchanges", "Balance overview", "Easy Spot Trading", "Secure wallet", "Networking & Follow Leaders", "AI Solutions", "Get Started Easily Today with our Beginner Plan"]}
+                    listItems={[
+                        "Real-time dashboard",
+                        "Access to Multiple exchanges",
+                        "Balance overview Easy Spot Trading", 
+                        "Secure wallet",
+                        "Networking & Follow Leaders",
+                        "AI Solutions",
+                        "Get Started Easily Today with our\nBeginner Plan"
+                    ]}
                     buttonText="Start Trading" />
                 <Upgrade
                     heading="100 $"
                     subheading="Pro starter"
-                    listItems={["Real-time dashboard", "Access to Multiple exchanges", "Balance overview", "Easy Spot Trading", "Secure wallet", "Networking & Follow Leaders", "AI Solutions", "Easily Trade up to $100K monthly"]}
+                    listItems={[
+                        "Real-time dashboard",
+                        "Access to Multiple exchanges",
+                        "Balance overview",
+                        "Easy Spot Trading Secure wallet", 
+                        "Networking & Follow Leaders",
+                        "AI Solutions",
+                        "Easily Trade up to $100K monthly"
+                    ]}
                     buttonText="Upgrade Now!" />
                 <Upgrade
                     heading="1000 $"
                     subheading="Premium"
-                    listItems={["Real-time dashboard", "Access to Multiple exchanges", "Balance overview", "Easy Spot Trading", "Secure wallet", "Networking & Follow Leaders", "AI Solutions", "Easily Trade up to $1M monthly"]}
+                    listItems={[
+                        "Real-time dashboard",
+                        "Access to Multiple exchanges",
+                        "Balance overview",
+                        "Easy Spot Trading",
+                        "Secure wallet",
+                        "Networking & Follow Leaders",
+                        "AI Solutions",
+                        "Easily Trade up to $1M monthly"
+                    ]}
                     buttonText="Upgrade Now!" />
                 <Upgrade
                     heading="10000 $"
                     subheading="Expert"
-                    listItems={["Real-time dashboard", "Access to Multiple exchanges", "Balance overview", "Easy Spot Trading", "Secure wallet", "Networking & Follow Leaders", "AI Solutions", "Easily Trade up to $10M monthly"]}
+                    listItems={[
+                        "Real-time dashboard",
+                        "Access to Multiple exchanges",
+                        "Balance overview",
+                        "Easy Spot Trading",
+                        "Secure wallet",
+                        "Networking & Follow Leaders",
+                        "AI Solutions",
+                        "Easily Trade up to $10M monthly"
+                    ]}
                     buttonText="Upgrade Now!" />
             </div>
             </div>
